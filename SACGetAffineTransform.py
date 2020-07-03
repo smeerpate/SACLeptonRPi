2cd4092b1cd866cf31b5b785557e1d080f5bf2cf
smeerpate <frederic.torreele@telenet.be>
2020-07-03 03:07:19 -0400

    Separate funtion for writing two images (top/bottom) to the frame buffer.

--- SACGetAffineTransform.py
+++ SACGetAffineTransform.py
@@ -25,6 +25,25 @@ fbCanvas = np.zeros((screenHeight, screenWidth, 4), dtype=np.uint8)
 tcCircles = []
 thCircles = []
 
+# fbSize is (width,height)
+def showInFrameBufferTopBottom(imageTop, imageBottom, fbSize):
+    imSize = (fbSize[0], fbSize[1]/2)
+    _a = np.ones((imSize[1],imSize[0]), dtype=np.uint8)*255
+    _imageTop = cv2.resize(imageTop, imSize, interpolation = cv2.INTER_AREA)
+    _imageBottom = cv2.resize(imageBottom, imSize, interpolation = cv2.INTER_AREA)
+    # Empty framebuffer content array BGRA
+    fbCont = np.zeros((screenHeight, screenWidth, 4), dtype=np.uint8)
+    # Place content in fb array
+    _b,_g,_r = cv2.split(_imageTop)
+    _imageTop = cv2.merge((_b,_g,_r,_a))
+    fbCont[0:imSize[1], 0:imSize[0]] = _imageTop
+    _b,_g,_r = cv2.split(_imageBottom)
+    _imageBottom = cv2.merge((_b,_g,_r,_a))
+    fbCont[(fbSize[1]/2):((fbSize[1]/2)+imSize[1]), 0:imSize[0]] = _imageBottom
+    # Write to frame buffer
+    with open('/dev/fb0', 'rb+') as _fBuf:
+        _fBuf.write(fbCont)
+
 
 try:
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -33,8 +52,10 @@ try:
         raw,_ = l.capture()
         cv2.normalize(raw, raw, 0, 65535, cv2.NORM_MINMAX)
         np.right_shift(raw, 8, raw)
-        thImage = cv2.resize(np.uint8(raw), singleOutputImageSize, interpolation = cv2.INTER_AREA)
-        tcImage = cv2.resize(tcImage, singleOutputImageSize, interpolation = cv2.INTER_AREA)
+#        thImage = cv2.resize(np.uint8(raw), singleOutputImageSize, interpolation = cv2.INTER_AREA)
+#        tcImage = cv2.resize(tcImage, singleOutputImageSize, interpolation = cv2.INTER_AREA)
+        thImage = np.uint8(raw) # 80x60
+        tcImage = tcImage # 640x480
         # find spots in both images
         del tcCircles[:]
         del thCircles[:]
@@ -66,16 +87,17 @@ try:
         tcImage = cv2.cvtColor(tcImage, cv2.COLOR_GRAY2BGR)
         thImage = cv2.cvtColor(thImage, cv2.COLOR_GRAY2BGR)
 
-        b,g,r = cv2.split(tcImage)
-        fbImageTop = cv2.merge((b,g,r,alpha))
-        fbCanvas[0:singleOutputImageSize[1], 0:singleOutputImageSize[0]] = fbImageTop
+        showInFrameBufferTopBottom(tcImage, thImage, (screenWidth, screenHeight))
+ #       b,g,r = cv2.split(tcImage)
+ #       fbImageTop = cv2.merge((b,g,r,alpha))
+ #       fbCanvas[0:singleOutputImageSize[1], 0:singleOutputImageSize[0]] = fbImageTop
 
-        b,g,r = cv2.split(thImage)
-        fbImageBottom = cv2.merge((b,g,r,alpha))
-        fbCanvas[(screenHeight/2):((screenHeight/2)+singleOutputImageSize[1]), 0:singleOutputImageSize[0]] = fbImageBottom
+ #       b,g,r = cv2.split(thImage)
+ #       fbImageBottom = cv2.merge((b,g,r,alpha))
+ #       fbCanvas[(screenHeight/2):((screenHeight/2)+singleOutputImageSize[1]), 0:singleOutputImageSize[0]] = fbImageBottom
 
-        with open('/dev/fb0', 'rb+') as fBuf:
-            fBuf.write(fbCanvas)
+ #       with open('/dev/fb0', 'rb+') as fBuf:
+ #           fBuf.write(fbCanvas)
 
         rawCapture.truncate()
         rawCapture.seek(0)
