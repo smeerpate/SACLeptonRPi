2a3a16954161bc3866a7e7506a82d9535700d139
smeerpate <frederic.torreele@telenet.be>
2020-10-24 11:09:19 -0400

    Add FPA and Aux temperature reading.

--- SACTempMeasure/LEPConfigure.c
+++ SACTempMeasure/LEPConfigure.c
@@ -15,6 +15,7 @@ int main()
     LEP_RESULT sResult;
     LEP_SYS_FLIR_SERIAL_NUMBER_T sSN;
     LEP_SYS_UPTIME_NUMBER_T sUpTime;
+    LEP_SYS_FPA_TEMPERATURE_CELCIUS_T sFPATempDegC;
 
     sResult = LEP_OpenPort(1, LEP_CCI_TWI, 400, &i2cPort);
     printf("sResult OpenPort: %d\n", sResult);
@@ -25,7 +26,13 @@ int main()
     printf("SerialNr: %d\n", sSN);
 
     sResult = LEP_GetSysCameraUpTime(&i2cPort, &sUpTime);
-    printf("Uptime: %d\n", sUpTime);
+    printf("Uptime: %fs\n", (float)sUpTime/1000.0);
+
+    sResult = LEP_GetSysFpaTemperatureCelcius(&i2cPort, &sFPATempDegC);
+    printf("FPA Temperature: %fdegC\n", (float)sFPATempDegC);
+
+    sResult = LEP_GetSysAuxTemperatureCelcius(&i2cPort, &sFPATempDegC);
+    printf("Aux Temperature: %fdegC\n", (float)sFPATempDegC);
 
     sResult = LEP_ClosePort(&i2cPort);
     printf("sResult ClosePort: %d\n", sResult);
