af0d8242a68fd25b6b4753343227bf6107898267
smeerpate <51826342+smeerpate@users.noreply.github.com>
2020-10-25 17:28:28 +0100

    Corrected indents

--- SACFaceFinder.py
+++ SACFaceFinder.py
@@ -36,37 +36,37 @@ class FaceFider:
 			# rect comes in a tuple (x,y,w,h).
 			# todo: check for biggest bounding box.
 			self.tcROI = rects[0]
-	    	return TRUE
-	    else:
-	    	# no faces found
-	    	self.tcROI = (-1,-1,-1,-1)
-	    	return FALSE
+	    		return TRUE
+	    	else:
+	    		# no faces found
+	    		self.tcROI = (-1,-1,-1,-1)
+	    		return FALSE
 	    		
 	####################################################
 	# Uses the transformation matrix to transform
 	# the true color ROI to a thermal image ROI.
 	####################################################    		
 	def getThFaceContours(self):  		
-    	# transform the coordinates from true color image space to thermal image space using the affine transform matrix M
-        # See https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html
-        # [[xt][yt]] = M . [[x][y][1]], with x,y=original coords and xt,yt=transformed coords.
-        #
-        # --->x
-        # |
-        # |   lt          rt
-        # V   x------------x
-        # y   |            |
-        #     |            |
-        #     |            |
-        #     x------------x
-        #     lb          rb
-        #
-        P_slt = np.array([[self.tcROI.x],[self.tcROI.y],[1]]) # 'slt' source left top vector
-        P_srt = np.array([[self.tcROI.x + self.tcROI.w],[self.tcROI.y],[1]]) # 'srt' source right top vector
-        P_slb = np.array([[self.tcROI.x],[self.tcROI.y + self.tcROI.h],[1]]) # 'slb' source left bottom vector
-        P_srb = np.array([[[self.tcROI.x + self.tcROI.w],[self.tcROI.y + self.tcROI.h],[1]]) # 'srb' source right bottom vector
-        P_dlt = np.dot(self.M, P_slt)
-        P_drt = np.dot(self.M, P_srt)
-        P_dlb = np.dot(self.M, P_slb)
-        P_drb = np.dot(self.M, P_srb)
-        thROIPoints = np.array([P_dlt, P_drt, P_dlb, P_drb], dtype=np.float32)
+    		# transform the coordinates from true color image space to thermal image space using the affine transform matrix M
+        	# See https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html
+         	# [[xt][yt]] = M . [[x][y][1]], with x,y=original coords and xt,yt=transformed coords.
+        	#
+        	# --->x
+        	# |
+        	# |   lt          rt
+        	# V   x------------x
+        	# y   |            |
+        	#     |            |
+        	#     |            |
+        	#     x------------x
+        	#     lb          rb
+        	#
+		P_slt = np.array([[self.tcROI.x],[self.tcROI.y],[1]]) # 'slt' source left top vector
+		P_srt = np.array([[self.tcROI.x + self.tcROI.w],[self.tcROI.y],[1]]) # 'srt' source right top vector
+		P_slb = np.array([[self.tcROI.x],[self.tcROI.y + self.tcROI.h],[1]]) # 'slb' source left bottom vector
+		P_srb = np.array([[[self.tcROI.x + self.tcROI.w],[self.tcROI.y + self.tcROI.h],[1]]) # 'srb' source right bottom vector
+		P_dlt = np.dot(self.M, P_slt)
+		P_drt = np.dot(self.M, P_srt)
+		P_dlb = np.dot(self.M, P_slb)
+		P_drb = np.dot(self.M, P_srb)
+		thROIPoints = np.array([P_dlt, P_drt, P_dlb, P_drb], dtype=np.float32)
