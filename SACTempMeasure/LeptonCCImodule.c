e6d48f2c0636bed7fa8ab24860bb9724b49b7698
smeerpate <frederic.torreele@telenet.be>
2020-10-26 15:30:43 -0400

    first pythonized LeptonCCI function working!

--- SACTempMeasure/LeptonCCImodule.c
+++ SACTempMeasure/LeptonCCImodule.c
@@ -8,6 +8,7 @@
 #include <LEPTON_SYS.h>
 #include <LEPTON_OEM.h>
 #include <LEPTON_Types.h>
+#include <LEPTON_RAD.h>
 
 PyObject* LeptonCCIError;
 LEP_CAMERA_PORT_DESC_T i2cPort;
@@ -34,12 +35,35 @@ static PyObject* LeptonCCI_RunRadFfc(PyObject* self) {
 
 
 // Method mapping table
+// Method definition object for this extension, these argumens mean:
+// ml_name: The name of the method
+// ml_meth: Function pointer to the method implementation
+// ml_flags: Flags indicating special features of this method, such as
+//          accepting arguments, accepting keyword arguments, being a
+//          class method, or being a static method of a class.
+// ml_doc:  Contents of this method's docstring
 static PyMethodDef LeptonCCI_methods[] = {
-    {"RunRadFfc", (PyFunction)LeptonCCI_RunRadFfc, METH_NOARGS, NULL},
+    {"RunRadFfc", (PyCFunction)LeptonCCI_RunRadFfc, METH_NOARGS, NULL},
     {NULL, NULL, 0, NULL}
 };
 
-// Initialisation function. Called when Python interpreter loads the module.
-PyMODINIT_FUNC initLeptonCCI() {
-    Py_InitModule3("RunRadFfc", LeptonCCI_methods, "Runs the radiometry flat field calibration.");
+// Module definition
+// The arguments of this structure tell Python what to call your extension,
+// what it's methods are and where to look for it's method definitions
+static struct PyModuleDef LeptonCCI_definition = {
+    PyModuleDef_HEAD_INIT,
+    "LeptonCCI",
+    "A Python wrapper around Flir Lepton embedded SDK.",
+    -1,
+    LeptonCCI_methods
+};
+
+
+// Module initialization
+// Python calls this function when importing your extension. It is important
+// that this function is named PyInit_[[your_module_name]] exactly, and matches
+// the name keyword argument in setup.py's setup() call.
+PyMODINIT_FUNC PyInit_LeptonCCI(void) {
+    Py_Initialize();
+    return PyModule_Create(&LeptonCCI_definition);
 }
