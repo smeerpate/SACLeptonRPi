38d2f295e552a29b3291c72a8281293ae4270004
Jason Pitteman <pittemanjason@hotmail.com>
2020-11-27 21:24:58 +0100

    ss

--- Main.py
+++ Main.py
@@ -66,8 +66,8 @@ for data in camera.capture_continuous(rawCapture, format="rgb", use_video_port=T
     
     shm.write(frame)
 
-    #if stateMachine.state == "WAIT_FOR_NO_FACE":
-        #time.sleep(5)
+    if stateMachine.state == "WAIT_FOR_NO_FACE":
+        time.sleep(5)
     
     
 
