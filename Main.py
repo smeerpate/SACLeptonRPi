4ccc7d9edf9ced4b02f5a4a787a75996887ccf10
Jason Pitteman <pittemanjason@hotmail.com>
2020-11-28 13:56:09 +0100

    log

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ from picamera import PiCamera
 settingsManager = SettingsManager()
 inputManager = InputManager(5, 6, 13)
 ledDriver = LedDriver(17, 27, 22)
-f = open("SACTemplog2.csv", "a")
+f = open("SACTemplog New.csv", "a")
 stateMachine = StateMachine(settingsManager, ledDriver, f)
 osd = OSD(inputManager, settingsManager)
 
