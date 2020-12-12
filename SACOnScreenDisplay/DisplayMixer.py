06f992e4e106cf9639b05c61f9331b314d496226
Jason Pitteman <pittemanjason@hotmail.com>
2020-12-12 17:18:50 +0100

    s

--- SACOnScreenDisplay/DisplayMixer.py
+++ SACOnScreenDisplay/DisplayMixer.py
@@ -43,25 +43,14 @@ class DisplayMixer(object):
         #start = int(round(time.time() * 1000))
         resizeFactor = 1.6875
         image = cv.flip(image, 0)
-        print("img size: " + str(image.shape))
+        #print("img size: " + str(image.shape))
         image = cv.resize(image, (1080, 810))
-        print("img size: " + str(image.shape))
+        #print("img size: " + str(image.shape))
         r_channel, g_channel, b_channel = cv.split(image)
         alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 255 #creating a dummy alpha channel image.
         img_RGBA = cv.merge((r_channel, g_channel, b_channel, alpha_channel))
 
-        #start = int(round(time.time() * 1000))
-        #slide = cv.imread(slideName)
-        #print("read from disk: " + str(int(round(time.time() * 1000)) - start) + "ms")
-        #start = int(round(time.time() * 1000))
-        #slide = cv.cvtColor(slide, cv.COLOR_RGB2RGBA)        
-        #print("cvt color: " + str(int(round(time.time() * 1000)) - start) + "ms")
-        #start = int(round(time.time() * 1000))
-        #slide = cv.flip(slide, 0) # maybe flipped on disk instead of doing it codewise???? because these slides are hardcoded so...
-        #print("flip took: " + str(int(round(time.time() * 1000)) - start) + "ms")
-        #reclame = np.zeros([1110, 1080, 4], dtype=np.uint8)
-        #reclame[:] = (0, 0, 255, 255)
-        print("slide size: " + str(slide.shape))
+        #print("slide size: " + str(slide.shape))
         start = int(round(time.time() * 1000))
         self.shm.write(np.vstack((slide, img_RGBA)))
         print("Show took: " + str(int(round(time.time() * 1000)) - start) + "ms")
