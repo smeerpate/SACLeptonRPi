c029efeb0b7cce1fcf32a76eacc9e165b97f125f
Jason Pitteman <pittemanjason@hotmail.com>
2020-12-23 16:21:06 +0100

    s

--- SACOnScreenDisplay/DisplayMixer.py
+++ SACOnScreenDisplay/DisplayMixer.py
@@ -37,7 +37,7 @@ class DisplayMixer(object):
         self.dontMove = cv.flip(self.dontMove, 0)
         #1080x1920
         self.transparent = np.full([1080, 1000, 4], 150, dtype=np.uint8)
-        self.transparent = np.vstack(self.transparent, np.full([1080, 920, 4], 240, dtype=np.uint8))
+        self.transparent = np.vstack((self.transparent, np.full([1080, 920, 4], 240, dtype=np.uint8)))
 
     def show(self, image, slide):     
         # image = 480(h)*640(w)
