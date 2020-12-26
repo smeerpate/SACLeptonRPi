5e906f64219e47a5b87da91bb92720d764d748ac
Jason Pitteman <pittemanjason@hotmail.com>
2020-12-26 14:16:28 +0100

    s

--- SACForeheadFinder.py
+++ SACForeheadFinder.py
@@ -62,9 +62,9 @@ class ForeheadFinder(RectangleOfInterestFinder):
         for detection in out.reshape(-1, 7):
             confidence = float(detection[2])
             if confidence > 0.5:
-                xmin = int(detection[3] * image.shape[1]) + 220
+                xmin = int(detection[3] * image.shape[1])
                 ymin = int(detection[4] * image.shape[0])
-                xmax = int(detection[5] * image.shape[1]) + 220
+                xmax = int(detection[5] * image.shape[1])
                 ymax = int(detection[6] * image.shape[0])
                 faceFound = True
 		        #print("xmin: " + str(xmin))
