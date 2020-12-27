5ec7178aa11bd080daad9bd173c1044c921b6d90
Jason Pitteman <pittemanjason@hotmail.com>
2020-12-27 19:07:33 +0100

    s

--- Main.py
+++ Main.py
@@ -12,49 +12,56 @@ from SACOnScreenDisplay.DisplayMixer import DisplayMixer
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 
-displayMixer = DisplayMixer()
-settingsManager = SettingsManager()
-inputManager = InputManager(5, 6, 13)
-ledDriver = LedDriver(17, 27, 22)
-f = open("/home/pi/SACLeptonRPi/Logging/SAC Temp Log.csv", "a")
-stateMachine = StateMachine(settingsManager, ledDriver, f, displayMixer)
-osd = OSD(inputManager, settingsManager, displayMixer)
+try:
+    displayMixer = DisplayMixer()
+    settingsManager = SettingsManager()
+    inputManager = InputManager(5, 6, 13)
+    ledDriver = LedDriver(17, 27, 22)
+    f = open("/home/pi/SACLeptonRPi/Logging/SAC Temp Log.csv", "a")
+    stateMachine = StateMachine(settingsManager, ledDriver, f, displayMixer)
+    osd = OSD(inputManager, settingsManager, displayMixer)
 
-# Target screen is 12", 1024x768 or 768x1024 in portrait mode
-screenWidth = 768
-screenHeight = 1024
-# Sensor size is 80x60
-sensorWidth = 80
-sensorHeight = 60
-# Lepton offset in degC
-corrVal = 0
-maxVal = 0
-feverThresh = 35.4
+    # Target screen is 12", 1024x768 or 768x1024 in portrait mode
+    screenWidth = 768
+    screenHeight = 1024
+    # Sensor size is 80x60
+    sensorWidth = 80
+    sensorHeight = 60
+    # Lepton offset in degC
+    corrVal = 0
+    maxVal = 0
+    feverThresh = 35.4
 
-camera = PiCamera()
-camera.resolution = (640, 480)
-rawCapture = PiRGBArray(camera, size=(640, 480))
-camera.meter_mode = 'spot'
-camera.framerate = 30
-time.sleep(0.5)
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+    camera.meter_mode = 'spot'
+    camera.framerate = 30
+    time.sleep(0.5)
 
-for data in camera.capture_continuous(rawCapture, format="rgb", use_video_port=True):
-    frame = data.array
-    frame = frame.copy()
-    if osd.isRunning() or inputManager.hasInput():
-        stateMachine.reset()
-        osd.run(frame)        
-    else:
-        stateMachine.run(frame)
+    for data in camera.capture_continuous(rawCapture, format="rgb", use_video_port=True):
+        frame = data.array
+        frame = frame.copy()
+        if osd.isRunning() or inputManager.hasInput():
+            stateMachine.reset()
+            osd.run(frame)        
+        else:
+            stateMachine.run(frame)
 
                 
-    rawCapture.truncate(0)    
-    
-    
+        rawCapture.truncate(0)   
 
-# When everything done, release the capture
-ledDriver.stop()
-displayMixer.stop()
-f.close()
-cap.release()
-cv.destroyAllWindows()
+    # When everything done, release the capture
+    ledDriver.stop()
+    displayMixer.stop()
+    f.close()
+    cap.release()
+    cv.destroyAllWindows()
+    
+except:
+    # When everything done, release the capture
+    ledDriver.stop()
+    displayMixer.stop()
+    f.close()
+    cap.release()
+    cv.destroyAllWindows()
