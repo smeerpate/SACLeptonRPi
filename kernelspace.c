2784f39a12891850bd24fa20f2173ee869a38ded
Jason Pitteman <pittemanjason@hotmail.com>
2021-02-17 16:53:50 +0100

    Create kernelspace.c

---
+++ kernelspace.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+#include <linux/gpio.h>
+
+int main(){
+	int ret = 0;
+	
+	
+	while(1){
+		ret = gpio_direction_output(16, 0);
+		fprintf(stderr, "gpio_direction_output -> 0 returned %d\n", ret);
+		
+		usleep(2000000);
+		
+		ret = gpio_direction_output(16, 1);
+		fprintf(stderr, "gpio_direction_output -> 1 returned %d\n", ret);
+	}
+
+	return 0;
+}
