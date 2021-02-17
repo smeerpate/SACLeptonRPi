f56d587ad76a695a5dc40e1a0f8a919c4b450705
Jason Pitteman <pittemanjason@hotmail.com>
2021-02-17 17:02:12 +0100

    Update kernelspace.c

--- kernelspace.c
+++ kernelspace.c
@@ -7,6 +7,9 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 #include <linux/gpio.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
 
 int main(){
 	int ret = 0;
