bce4ba5f6867528cae148451d1b40c0bb393ee58
Jason Pitteman <pittemanjason@hotmail.com>
2021-02-17 11:14:44 +0100

    testioctl.c: Added while loop and delay for simulating purposes

--- testioctl.c
+++ testioctl.c
@@ -13,43 +13,48 @@ int main(){
 	struct gpiohandle_data data;
 	char chrdev_name[20] = "/dev/gpiochip0";
 	int fd, ret;
+	
+	while(1){
+		fd = open(chrdev_name, 0);
+		if (fd == -1) {
+			fprintf(stderr, "Failed to open %s\n", chrdev_name);
+			return ret;
+		}
+		fprintf(stderr, "File descriptor %d", fd);
 
-	fd = open(chrdev_name, 0);
-	if (fd == -1) {
-		fprintf(stderr, "Failed to open %s\n", chrdev_name);
-		return ret;
+		req.lineoffsets[0] = 16;
+		req.flags = GPIOHANDLE_REQUEST_OUTPUT;
+		memcpy(req.default_values, &data, sizeof(req.default_values));
+		strcpy(req.consumer_label, "led_gpio");
+		req.lines = 1;
+
+		ret = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &req);
+		if (ret == -1) {
+			fprintf(stderr, "Failed to issue GET LINEHANDLE IOCTL (%d)\n",
+				ret);
+		}
+		if (close(fd) == -1)
+			perror("Failed to close GPIO character device file");
+		fprintf(stderr, "Req file descriptor %d", req.fd);
+
+		data.values[0] = 1;
+		ret = ioctl(req.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
+		if (ret == -1) {
+			fprintf(stderr, "Failed to issue %s (%d)\n",
+					"GPIOHANDLE_SET_LINE_VALUES_IOCTL", ret);
+		}
+
+		usleep(1000000);
+		
+		/*  release line */
+		ret = close(req.fd);
+		if (ret == -1) {
+			perror("Failed to close GPIO LINEHANDLE device file");
+		}	
+		
+		// Sleep to simulate
+		usleep(3000000);
 	}
-	fprintf(stderr, "File descriptor %d", fd);
-
-	req.lineoffsets[0] = 16;
-	req.flags = GPIOHANDLE_REQUEST_OUTPUT;
-	memcpy(req.default_values, &data, sizeof(req.default_values));
-	strcpy(req.consumer_label, "led_gpio");
-	req.lines = 1;
-
-	ret = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &req);
-	if (ret == -1) {
-		fprintf(stderr, "Failed to issue GET LINEHANDLE IOCTL (%d)\n",
-			ret);
-	}
-	if (close(fd) == -1)
-		perror("Failed to close GPIO character device file");
-	fprintf(stderr, "Req file descriptor %d", req.fd);
-	usleep(1000000);
-
-	data.values[0] = 1;
-	ret = ioctl(req.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
-	if (ret == -1) {
-		fprintf(stderr, "Failed to issue %s (%d)\n",
-				"GPIOHANDLE_SET_LINE_VALUES_IOCTL", ret);
-	}
-
-	usleep(1000000);
-	/*  release line */
-	ret = close(req.fd);
-	if (ret == -1) {
-		perror("Failed to close GPIO LINEHANDLE device file");
-	}	
 
 	return 0;
 }
