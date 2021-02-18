60b7cc175da386ec29efe3491937a13a547e61ab
Jason Pitteman <pittemanjason@hotmail.com>
2021-02-18 17:17:14 +0100

    Update resetlgpio.c

--- resetlgpio.c
+++ resetlgpio.c
@@ -11,15 +11,21 @@ int main(int argc, char *argv[])
 
    /* get a handle to the GPIO */
    h = lgGpiochipOpen(0);
+   
+   fprintf(stdout, "Handle: %d", h);
 
    /* claim some GPIO for OUTPUT */
-   lgGpioClaimOutput(h, lFlags, OUT1, 1); /* initial level 1 */
+   int response = lgGpioClaimOutput(h, lFlags, OUT1, 1); /* initial level 1 */
+   
+   fprintf(stdout, "Claim response: %d\n", response);
 
    while (1)
    {
 	  usleep(1000000);
+	  fprintf(stdout, "Low\n");
 	  lgGpioWrite(h, OUT1, 0);
 	  usleep(1000000);
+	  fprintf(stdout, "High\n");
 	  lgGpioWrite(h, OUT1, 1);
    }
 }
